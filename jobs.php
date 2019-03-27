017b2b6e63c81ad66d20255bbaac482e78ae1c23
DevEloperNik <13nm77@queensu.ca>
2019-03-27 13:40:41 -0400

    adding pages

---
+++ jobs.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>CompRecCon - Jobs</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="home.css" type="text/css" rel="stylesheet">
+    <link rel="icon" href="images/favicon.ico" />
+    <title>CMPE 332 Project: Group 95</title>
+</head>
+
+<body>
+    <div class="head1"></div>
+
+    <section>
+        <div class="sidenav">
+            <a href="index.html">Home</a>
+            <a href="schedule.html">Schedule</a>
+            <a href="committees.php">Committees</a>
+            <a href="sponsors.php">Sponsors</a>
+            <a href="attendees.php">Attendees</a>
+            <a href="hotel.php">Hotel</a>
+            <a href="jobs.php" class="active">Jobs</a>
+            <a href="finances.php">Finances</a>
+        </div>
+        <div class="content">
+            <h1>Jobs</h1>
+        </div>
+
+    </section>
+
+    <footer>
+        <p></p>
+    </footer>
+
+</body>
+
+</html>
